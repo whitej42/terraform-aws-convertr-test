7e857f4a8e66c5a909c5d019a5443c6222af4681
ClassicSmej <jamesw.white96@gmail.com>
2025-05-12 17:07:23 +0100

    remove endpoint policy for testing

--- security_groups.tf
+++ security_groups.tf
@@ -1,5 +1,9 @@
 resource "aws_security_group" "file_upload_lambda_sg" {
     name = "${local.env}-fileUploadLambda-sg"
+    description = "Allow Lambda Function to Upload via S3 Endpoint"
+    vpc_id = aws_vpc.demo_vpc.id
 
-    tags = local.default_tags
+    tags = merge(local.default_tags,{
+        Name = "${local.env}-fileUploadLambda-sg"
+    })
 }

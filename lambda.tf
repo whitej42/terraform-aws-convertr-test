d5c916d1d9fb31839620c1a5492ee481de0ce285
ClassicSmej <jamesw.white96@gmail.com>
2025-05-12 16:59:59 +0100

    deploy demo vpc

--- lambda.tf
+++ lambda.tf
@@ -15,10 +15,21 @@ resource "aws_lambda_function" "file_upload_lambda_function" {
     filename = data.archive_file.file_upload_lambda_zip.output_path
     source_code_hash = data.archive_file.file_upload_lambda_zip.output_base64sha256
     architectures = [ "x86_64" ]
-    handler = "index.handler"
+    handler = "file_upload.lambda_handler"
     package_type = "Zip"
     runtime = "python3.12"
     role = aws_iam_role.file_upload_lambda_role.arn
+
+    vpc_config {
+        subnet_ids         = [aws_subnet.demo_private_subnet.id]
+        security_group_ids = [aws_security_group.lambda_sg.id]
+    }
+
+    environment {
+      variables = {
+        BUCKET_NAME = aws_s3_bucket.file_upload_bucket.id
+      }
+    }
 
     tags = local.default_tags
 }
@@ -30,13 +41,5 @@ resource "aws_lambda_permission" "file_upload_lambda_api_permissions" {
   function_name = aws_lambda_function.file_upload_lambda_function.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn    = "${aws_api_gateway_rest_api.upload_rest_api.execution_arn}/*/*"
-}
-
-# Layers for additional Python packages
-resource "aws_lambda_layer_version" "filetype_lambda_layer" {
-  filename   = "${path.module}/src/lambda/file_upload/filetype_layer"
-  layer_name = "filetype_layer_name"
-
-  compatible_runtimes = ["python3.13"]
+  source_arn = "${aws_api_gateway_rest_api.upload_rest_api.execution_arn}/*/POST/*"
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "repo_url" {
   type        = string
   description = "GitHub repository URL"
 }
+
+variable "cidr_block" {
+  type = string
+  description = "Demo VPC CIDR Block"
+}
+
+variable "private_subnet_cidr" {
+  type = string
+  description = "Demo Private Subnet VPC CIDR Block"
+}

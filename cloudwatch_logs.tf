8de771ec9fd71a39b00c06b853e73ee959f78c7e
ClassicSmej <jamesw.white96@gmail.com>
2025-05-12 15:19:54 +0100

    remove bucket acl

--- cloudwatch_logs.tf
+++ cloudwatch_logs.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_log_group" "file_upload_lambda_log_group" {
   log_group_class = "STANDARD"
-  name = "aws/lambda/${local.env}-convertrFileUpload-function"
+  name = "lambda/${local.env}-convertrFileUpload-function"
   retention_in_days = 0
   skip_destroy = true
   tags = local.default_tags
